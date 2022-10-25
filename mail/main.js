cd355a1d317559e1c11fab6aa8e71483b87990a6
NicholasPeara92 <nicholas.peara@gmail.com>
2022-10-25 16:19:08 +0200

    Aggiunge console messaggio nella console

--- mail/main.js
+++ mail/main.js
@@ -7,14 +7,14 @@ const mail = prompt("Inserisci la tua e-mail");
 // controlla che sia nella lista di chi può accedere,
 let index = mails.indexOf(mail);
 
-console.log(index);
-
 // stampa un messaggio appropriato sull’esito del controllo.
 
 if ( index >= 0 ) {
     alert("La mail indicata è presente");
+    console.log("La mail indicata è presente");
 } else {
     alert("La mail indicata non è presente");
+    console.log("La mail indicata non è presente");
 
 }
 
